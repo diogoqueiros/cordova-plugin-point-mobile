1b52c00ad1ff9d731bd1dcb7a645f5c27e5e8516
Diogo Queirós <diogoqueiros@outlook.com>
2021-01-03 02:05:16 +0000

    feat(plugin): add js file

---
+++ www/pointmobile.js
@@ -0,0 +1,8 @@
+module.exports = {
+    scan: function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'PointMobile', 'scan', []);
+    },
+    cancel: function () {
+        exec(null, null, 'PointMobile', 'cancel', []);
+    }
+};
